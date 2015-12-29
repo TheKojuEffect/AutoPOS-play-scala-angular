ca3a625f2861d31330226922b5ede61e91c7b2b3
Kapil Koju <kplx2.0@gmail.com>
2015-12-29 06:50:45 +0545

    Set no inspection for TypeScriptValidateTypes

--- ui/autopos/bootstrap.ts
+++ ui/autopos/bootstrap.ts
@@ -3,6 +3,7 @@ import {bootstrap} from 'angular2/platform/browser';
 import {ROUTER_PROVIDERS, APP_BASE_HREF, LocationStrategy, HashLocationStrategy} from 'angular2/router';
 import {AppCmp} from './components/app/app';
 
+//noinspection TypeScriptValidateTypes
 bootstrap(AppCmp, [
   provide(APP_BASE_HREF, { useValue: '<%= APP_ROOT %>' } ),
   ROUTER_PROVIDERS,
