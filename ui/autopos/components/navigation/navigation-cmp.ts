9f66144ded08e6d5ffa5ba697058afb652ca3be5
Kapil Koju <kplx2.0@gmail.com>
2015-12-29 07:47:52 +0545

    Set active class if component is active

--- ui/autopos/components/navigation/navigation-cmp.ts
+++ ui/autopos/components/navigation/navigation-cmp.ts
@@ -1,11 +1,22 @@
 import {Component} from 'angular2/core';
-import {ROUTER_DIRECTIVES} from 'angular2/router';
+import {Location, ROUTER_DIRECTIVES} from 'angular2/router';
 
 @Component({
   selector: 'navigation',
   templateUrl: './components/navigation/navigation.html',
-  directives:[ROUTER_DIRECTIVES]
+  directives: [ROUTER_DIRECTIVES]
 })
 
 export class NavigationCmp {
+
+  constructor(private _location:Location) {
+  }
+
+  public getPath() {
+    return this._location.path();
+  }
+
+  public hasPath(route:string):boolean {
+    return this.getPath().indexOf(route) !== -1;
+  }
 }
