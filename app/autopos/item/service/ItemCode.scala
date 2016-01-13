e632d47383d02b61c9d8f61b8a80fb6ecd151e89
Kapil Koju <kplx2.0@gmail.com>
2016-01-13 21:28:52 +0545

    Refactor to take Int id

--- app/autopos/item/service/ItemCode.scala
+++ app/autopos/item/service/ItemCode.scala
@@ -6,27 +6,27 @@ private[service] object ItemCode {
 
   private val base = alpha.length
 
-  private val firstChar = '3'
+  private val firstChar = alpha.charAt(0)
 
-  def apply(encodedInput: String) = decode(encodedInput)
+  def apply(code: String) = decode(code)
 
-  def apply(decodedInput: Long) = encode(decodedInput)
+  def apply(id: Int) = encode(id)
 
-  def encode(input: Long) = {
-    def enc(in: Long, acc: String): String =
+  def encode(id: Int) = {
+    def enc(in: Int, acc: String): String =
       if (in < 1) acc
-      else enc(in / base, alpha((in % base).toInt) + acc)
+      else enc(in / base, alpha(in % base) + acc)
 
-    enc(input, "")
+    enc(id, "")
   }
 
-  def decode: PartialFunction[String, Long] = {
-    case s: String if s.length == 0 => 0
-    case s: String if s.head != firstChar => {
-      val in = s.reverse
+  def decode: PartialFunction[String, Int] = {
+    case code: String if code.length == 0 => 0
+    case code: String if code.head != firstChar => {
+      val in = code.reverse
 
-      def dec(idx: Int, acc: BigInt): Long =
-        if (idx == in.length) acc.toLong
+      def dec(idx: Int, acc: BigInt): Int =
+        if (idx == in.length) acc.toInt
         else dec(idx + 1, acc + alpha.indexOf(in(idx)) * BigInt(base).pow(idx))
 
       dec(0, 0)
