afb32fc90b58fdedccd57e960ac911d63bb9d068
Kapil Koju <kplx2.0@gmail.com>
2016-03-15 22:41:11 +0545

    Move item code function to item_table sql file

--- conf/db/migration/default/V4__item_table.sql
+++ conf/db/migration/default/V4__item_table.sql
@@ -10,6 +10,7 @@ CREATE TABLE item (
   remarks      VARCHAR(250)
 );
 
+-- Start the id from 789 so that item code is at least 3 characters length
 ALTER SEQUENCE item_id_seq RESTART WITH 789;
 
 CREATE TABLE item_tag (
@@ -17,3 +18,54 @@ CREATE TABLE item_tag (
   tag_id  BIGINT NOT NULL REFERENCES tag (id),
   CONSTRAINT item_tag_pkey PRIMARY KEY (item_id, tag_id)
 );
+
+-- Function to get item code from item id
+CREATE FUNCTION item_code(id BIGINT)
+  RETURNS VARCHAR(14) AS $code$
+
+DECLARE
+  alphabet VARCHAR(24);
+  base     INTEGER DEFAULT 0;
+  code     VARCHAR(14);
+  divisor  BIGINT;
+  mod      INTEGER DEFAULT 0;
+
+BEGIN
+  alphabet := '3KMEQPNHABTGCWUVRYZFSXJD';
+  base := char_length(alphabet);
+  code := '';
+
+  WHILE id >= base LOOP
+    divisor := (id / base) :: BIGINT;
+    mod := (id - (base * divisor)) :: INTEGER;
+    code := concat(substring(alphabet FROM mod + 1 FOR 1), code);
+    id := divisor;
+  END LOOP;
+
+  IF id > 0
+  THEN
+    code = concat(substring(alphabet FROM (id :: INTEGER) + 1 FOR 1), code);
+  END IF;
+
+  RETURN (code);
+
+END; $code$
+LANGUAGE PLPGSQL;
+
+-- Trigger function to set item code
+CREATE FUNCTION set_item_code()
+  RETURNS TRIGGER AS
+$BODY$
+BEGIN
+  NEW.code := item_code(NEW.id);
+  RETURN NEW;
+END;
+$BODY$
+LANGUAGE PLPGSQL;
+
+-- Trigger to set item code before insert
+CREATE TRIGGER set_item_code_trigger
+BEFORE INSERT
+ON item
+FOR EACH ROW
+EXECUTE PROCEDURE set_item_code();
